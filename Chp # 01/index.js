a69ec90727bb44117e53202a2289e374c2ea7390
maryamasif2008 <marayam.asif123@gmail.com>
2024-08-28 00:06:47 +0500

    added index.html and  JS file

---
+++ Chp # 01/index.js
@@ -0,0 +1,25 @@
+                                      //CHAPTER # 01
+                                      
+                                      /* Question 01 */
+ alert("Welcome to our website");                                     
+
+                                     /* Question 02 */
+ alert("Sorry! Please enter a valid password.")
+ 
+                                     /* Question 03 */
+alert("Welcome to JS Land... \n Hppy Codding!");
+
+                                    /* Questio 04 */
+alert("Welcome to JS Land...");
+alert("Happy Codding! \n Prevnt this page from creating additional dialogs.")     
+
+ var message ="Hello... I can run JS through my web browser's console";
+ console.log(message)                                        
+                                    
+                                    /* Question 05 */
+
+
+                                    /* Question 06 */
+ Head   //correct answer 
+
+                              
